a44afb98b3f16002595dabe99f7d0e5d0ebbcd1f
interrupter <alex.repin@mail.ru>
2021-06-04 11:19:37 +0300

    added: locales support

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 
 module.exports = {
+  name: 'not-inform',
   Inform: require('./inform.js'),
   Supply: require('./supply.js'),
   Sink: require('./sink.js'),
   Rule: require('./rule.js'),
   paths: {
-    controllers:  __dirname + '/controllers'
+    controllers:  __dirname + '/controllers',
+    locales:  __dirname + '/locales'
   }
 };
